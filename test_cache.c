ecd7468ec243dd3783dad80fe17075108511dca0
Zheng Zhong <tonitick@hotmail.com>
2020-05-14 01:47:29 +0800

    add cache test

--- test_cache.c
+++ test_cache.c
@@ -1,21 +1,52 @@
 #include "cache.h"
 
+void display(struct CacheQueue* queue, struct Hash* hash) {
+    printf("lru queue id in order / reverse order):\n");
+    struct CacheNode* traverse = queue->front;
+    while(traverse != NULL) {
+        printf("%d ", traverse->block_id);
+        traverse = traverse->queue_next;
+    }
+    printf("\n");
+    traverse = queue->rear;
+    while(traverse != NULL) {
+        printf("%d ", traverse->block_id);
+        traverse = traverse->queue_prev;
+    }    
+    printf("\nhash buckets id in order / reverse order:\n");
+    for (int i = 0; i < hash->hash_capacity; i++) {
+        printf("hash bucket %d:\n", i);
+        traverse = hash->buckets[i];
+        while(traverse != NULL) {
+            printf("%d ", traverse->block_id);
+            traverse = traverse->hash_next;
+        }
+        printf("\n");
+        traverse = hash->buckets[i];
+        while(traverse != NULL && traverse->hash_next != NULL) {
+            traverse = traverse->hash_next;
+        }
+        while(traverse != NULL) {
+            printf("%d ", traverse->block_id);
+            traverse = traverse->hash_prev;
+        }
+        printf("\n");
+    }
+}
+
 int main() {
-    struct CacheQueue* q = create_cache_queue(4);
+    struct CacheQueue* queue = create_cache_queue(4);
 
     struct Hash* hash = create_hash_table(10);
 
-    get_block_cache(q, hash, 1);
-    get_block_cache(q, hash, 2);
-    get_block_cache(q, hash, 3);
-    get_block_cache(q, hash, 1);
-    get_block_cache(q, hash, 4);
-    get_block_cache(q, hash, 5);
+    get_block_cache(queue, hash, 1);
+    get_block_cache(queue, hash, 2);
+    get_block_cache(queue, hash, 3);
+    get_block_cache(queue, hash, 1);
+    get_block_cache(queue, hash, 4);
+    get_block_cache(queue, hash, 5);
 
-    printf("%d ", q->front->block_id);
-    printf("%d ", q->front->queue_next->block_id);
-    printf("%d ", q->front->queue_next->queue_next->block_id);
-    printf("%d ", q->front->queue_next->queue_next->queue_next->block_id);
+    display(queue, hash);
 
     return 0; 
 }

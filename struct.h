5c1da4a857e264d36acc86812bbf811ff005957c
Zheng Zhong <tonitick@hotmail.com>
2020-05-15 19:21:02 +0800

    add some macros

--- struct.h
+++ struct.h
@@ -2,9 +2,12 @@
 Authors:
 Zheng Zhong
 
-Data structures and utility functions for toyfs file system 
+Data structures and utility functions for accessing ToyFS
 */
 #include "cache.h"
+
+#define SIZE_BLOCK 512
+
 struct SuperBlock {
     unsigned int size_ibmap;
     unsigned int size_dbmap;
@@ -12,11 +15,37 @@ struct SuperBlock {
     unsigned int size_filename;
     unsigned int root_inum;
     unsigned int num_disk_ptrs_per_inode;
-};
-struct SuperBlock superblock;
+} superblock;
 
 #define SIZE_IBMAP superblock.size_ibmap
 #define SIZE_DBMAP superblock.size_dbmap
+#define SIZE_INODE superblock.size_inode
+#define SIZE_FILENAME superblock.size_filename
+#define ROOT_INUM superblock.root_inum
+#define NUM_DISK_PTRS_PER_INODE superblock.num_disk_ptrs_per_inode
+
+#define NUM_INODE (SIZE_IBMAP * 8)
+#define NUM_DATA_BLKS (SIZE_DBMAP * 8)
+#define NUM_BLKS_SUPERBLOCK 8 // set to 1 page = 8 blocks
+#define NUM_BLKS_IMAP (SIZE_IBMAP / SIZE_BLOCK)
+#define NUM_BLKS_DMAP (SIZE_DBMAP / SIZE_BLOCK)
+#define NUM_BLKS_INODE_TABLE (SIZE_INODE * NUM_INODE / SIZE_BLOCK)
+
+#define SUPERBLOCK_START_BLK 0
+#define IMAP_START_BLK NUM_BLKS_SUPERBLOCK
+#define DMAP_START_BLK (IMAP_START_BLK + NUM_BLKS_IMAP)
+#define INODE_TABLE_START_BLK (DMAP_START_BLK + NUM_BLKS_DMAP)
+#define DATA_REG_START_BLK (INODE_TABLE_START_BLK + NUM_BLKS_INODE_TABLE)
+
+#define SIZE_DATA_BLK_PTR 4 // size of data block pointers
+#define NUM_PTR_PER_BLK (SIZE_BLOCK / SIZE_DATA_BLK_PTR)
+#define NUM_FIRST_LEV_PTR_PER_INODE (NUM_DISK_PTRS_PER_INODE - 2)
+#define NUM_SECOND_LEV_PTR_PER_INODE NUM_PTR_PER_BLK
+#define NUM_THIRD_LEV_PTR_PER_INODE NUM_PTR_PER_BLK * NUM_PTR_PER_BLK
+#define NUM_FIRST_TWO_LEV_PTR_PER_INODE (NUM_FIRST_LEV_PTR_PER_INODE + NUM_SECOND_LEV_PTR_PER_INODE)
+#define NUM_ALL_LEV_PTR_PER_INODE (NUM_FIRST_LEV_PTR_PER_INODE + NUM_SECOND_LEV_PTR_PER_INODE + NUM_THIRD_LEV_PTR_PER_INODE)
+
+
 
 // bool inode_bitmap[SIZE_IBMAP];
 // bool data_bitmap[SIZE_DBMAP];
--- test_struct.c
+++ test_struct.c
@@ -1,8 +1,8 @@
 #include "struct.h"
 
 int main() {
-    superblock.size_ibmap = 384; // 4 pages = 384 blocks
-    superblock.size_dbmap = 384; // 4 pages = 384 blocks
+    superblock.size_ibmap = 196608; // 4 pages = 384 blocks = 196608 bytes = 1572864 bits
+    superblock.size_dbmap = 196608; // 4 pages = 384 blocks = 196608 bytes = 1572864 bits
     superblock.size_inode = 32; // 32 bytes
     superblock.size_filename = 12; // 12 byes
     superblock.root_inum = 0;
@@ -10,6 +10,30 @@ int main() {
 
     printf("SIZE_IBMAP = %d\n", SIZE_IBMAP);
     printf("SIZE_DBMAP = %d\n", SIZE_DBMAP);
+    printf("SIZE_INODE = %d\n", SIZE_INODE);
+    printf("SIZE_FILENAME = %d\n", SIZE_FILENAME);
+    printf("ROOT_INUM = %d\n", ROOT_INUM);
+    printf("NUM_DISK_PTRS_PER_INODE = %d\n", NUM_DISK_PTRS_PER_INODE);
+    printf("NUM_INODE = %d\n", NUM_INODE);
+    printf("NUM_DATA_BLKS = %d\n", NUM_DATA_BLKS);
+    printf("NUM_BLKS_SUPERBLOCK = %d\n", NUM_BLKS_SUPERBLOCK);
+    printf("NUM_BLKS_IMAP = %d\n", NUM_BLKS_IMAP);
+    printf("NUM_BLKS_DMAP = %d\n", NUM_BLKS_DMAP);
+    printf("NUM_BLKS_INODE_TABLE = %d\n", NUM_BLKS_INODE_TABLE);
+    printf("SUPERBLOCK_START_BLK = %d\n", SUPERBLOCK_START_BLK);
+    printf("IMAP_START_BLK = %d\n", IMAP_START_BLK);
+    printf("DMAP_START_BLK = %d\n", DMAP_START_BLK);
+    printf("INODE_TABLE_START_BLK = %d\n", INODE_TABLE_START_BLK);
+    printf("DATA_REG_START_BLK = %d\n", DATA_REG_START_BLK);
+
+    printf("SIZE_DATA_BLK_PTR = %d\n", SIZE_DATA_BLK_PTR);
+    printf("NUM_PTR_PER_BLK = %d\n", NUM_PTR_PER_BLK);
+    printf("NUM_FIRST_LEV_PTR_PER_INODE = %d\n", NUM_FIRST_LEV_PTR_PER_INODE);
+    printf("NUM_SECOND_LEV_PTR_PER_INODE = %d\n", NUM_SECOND_LEV_PTR_PER_INODE);
+    printf("NUM_THIRD_LEV_PTR_PER_INODE = %d\n", NUM_THIRD_LEV_PTR_PER_INODE);
+    printf("NUM_FIRST_TWO_LEV_PTR_PER_INODE = %d\n", NUM_FIRST_TWO_LEV_PTR_PER_INODE);
+    printf("NUM_ALL_LEV_PTR_PER_INODE = %d\n", NUM_ALL_LEV_PTR_PER_INODE);
+
 
     return 0;
 }
